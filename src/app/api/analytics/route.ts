5fc1196c6ead71006582702a845cc4424055bb6a
Csaba Polyak <csiberius@gmail.com>
2024-01-25 12:28:24 +0000

    edge v0.2

--- src/app/api/analytics/route.ts
+++ src/app/api/analytics/route.ts
@@ -5,7 +5,7 @@ import { env } from '@/env'
 import dayjs from '@/utils/dayjs'
 
 export const dynamic = 'force-dynamic'
-
+export const runtime = 'edge'
 const prisma = new PrismaClient({
   datasourceUrl: env.UMAMI_DATABASE_URL
 })
